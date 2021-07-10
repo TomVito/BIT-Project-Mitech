1f19f254bb3f74c539405974d3976e92c1ffbb7a
TomVito <t.vitonis@gmail.com>
2021-07-10 11:09:06 +0300

    animations

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -12,30 +12,74 @@ document.getElementById("atidarymoNuoroda").onclick = function () {
 
 // close cookies
 
+/*
+
 document.getElementById("sutinku").onclick = function () {
   var cookies = document.getElementById("slapukai");
 
   cookies.classList.add("closed");
 };
 
+*/
+
+// jQuery animations
+
 jQuery(document).ready(function () {
+  jQuery("#sutinku").click(function () {
+    jQuery("#slapukai").fadeOut(2000, function () {});
+  });
+
   jQuery("#btn-hero").click(function () {
     jQuery(".btn2").addClass("btn2-hero-show");
     jQuery("#img-hero").animate({
       opacity: 0.5,
+      left: "-=100px",
     });
     jQuery("#img-under").animate({
-        opacity: 0.5,
-      });
+      opacity: 0.5,
+    });
   });
 
   jQuery("#btn2-hero").click(function () {
     jQuery(".btn2").removeClass("btn2-hero-show");
     jQuery("#img-hero").animate({
       opacity: 1,
+      left: "0px",
     });
     jQuery("#img-under").animate({
-        opacity: 1,
-      });
+      opacity: 1,
+    });
+  });
+
+  jQuery("#arrow1").click(function () {
+    jQuery("#arrow1").animate(
+      {
+        bottom: "350px",
+      },
+      2000,
+      "linear"
+    );
+  });
+
+  jQuery("#arrow2").click(function () {
+    jQuery("#arrow2").animate(
+      {
+        bottom: "175px",
+        left: "-20px",
+      },
+      2000,
+      "linear"
+    );
+  });
+
+  jQuery("#arrow3").click(function () {
+    jQuery("#arrow3").animate(
+      {
+        height: "250px",
+        left: "+=220",
+      },
+      2000,
+      "linear"
+    );
   });
 });
